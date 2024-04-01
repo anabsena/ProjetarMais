a90614e0e2919503be908da40e42642b81b682c9
anabsena <anabsenaf@gmail.com>
2024-04-01 16:15:50 -0300

    sobre nós responsvo

--- src/screens/Public/home/Home.tsx
+++ src/screens/Public/home/Home.tsx
@@ -115,48 +115,51 @@ export const Home = () => {
 
         </div>
       </div>
-      <div className="w-full sm:h-56 relative flex justify-center">
-        <img src="img/top-sobre-nos.svg" className="w-full object-cover absolute z-10" alt="" />
-        <div className="flex flex-col items-center z-30">
-          <h1 className="uppercase text-[#27264D] font-bold text-4xl mt-24" style={{ fontFamily: "Adam, sans-serif" }}>
-            Quem somos
-          </h1>
-          <img src="img/separador-title.svg" alt="" />
-        </div>
-      </div>
-      <div className="w-full  bg-[#9BA1D1]  flex flex-col items-center justify-center relative sm:gap-12">
-        <img src="img/Icon-project.svg" className=" z-30 absolute top-0 right-0 hidden md:flex" alt="" />
-        <img src="img/Icon-planta.svg" className=" z-30 absolute bottom-2 left-8 hidden md:flex" alt="" />
-        <img src="img/Detalhes.svg" className=" z-30 absolute hidden w-56 -mt-12 md:flex" alt="" />
-        <div className="h-full z-30 text-black flex flex-col mt-8 sm:mt-0 items-center  justify-center">
-          <div className="flex items-center w-full gap-4 flex-col-reverse justify-center md:flex-row">
-            <div className="w-[410px] md:h-[537px] p-4 relative flex justify-center md:block">
-              <img src="img/mais_azul.svg" className="absolute top-0 left-0 w-36 hidden md:flex" alt="" />
-              <img src="img/Andressa.jpg" className="md:absolute bottom-0 right-0 w-80 h-[450px] object-cover" alt="" />
-            </div>
-            <div className="w-[395px] p-4">
-              <h1 style={{ fontFamily: "Alice", fontSize: "36px" }}>Andressa Belo</h1>
-              <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour
-                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</p>
-            </div>
+      <div className="flex flex-col">
+
+        <div className="w-full sm:h-56 relative flex justify-center">
+          <img src="img/top-sobre-nos.svg" className="w-full object-cover absolute z-10" alt="" />
+          <div className="flex flex-col items-center z-30">
+            <h1 className="uppercase text-[#27264D] font-bold text-4xl mt-12 sm:mt-24" style={{ fontFamily: "Adam, sans-serif" }}>
+              Quem somos
+            </h1>
+            <img src="img/separador-title.svg" alt="" />
           </div>
-          <div className="flex items-center md:-mt-20  flex-col justify-center md:flex-row">
-
-            <div className="w-[395px] p-4">
-              <h1 style={{ fontFamily: "Alice", fontSize: "36px" }}>Monica Goes</h1>
-              <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour
-                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</p>
-            </div>
-            <div className="w-[395px] md:h-[537px] p-4 relative flex justify-center sm:block">
-              <img src="img/mais_branco.svg" className="absolute top-0 right-0 w-36 hidden md:flex" alt="" />
-              <img src="img/Monica.jpg" className="md:absolute bottom-0 left-0 w-80 h-[450px] object-cover" alt="" />
-            </div>
-          </div>
-
         </div>
-        <Button variant={"inverseTwo"} size={"lg"}>Saiba mais <HiArrowSmRight /></Button>
-        <div>
+        <div className="w-full  bg-[#9BA1D1] flex flex-col items-center justify-center relative sm:gap-12">
+          <img src="img/Icon-project.svg" className=" z-30 absolute top-0 right-0 hidden md:flex" alt="" />
+          <img src="img/Icon-planta.svg" className=" z-30 absolute bottom-2 left-8 hidden md:flex" alt="" />
+          <img src="img/Detalhes.svg" className=" z-30 absolute hidden w-56 -mt-12 md:flex" alt="" />
+          <div className="h-full z-30 text-black flex flex-col mt-8 sm:mt-0 items-center  justify-center">
+            <div className="flex items-center w-full gap-4 flex-col-reverse justify-center md:flex-row">
+              <div className="sm:w-[410px] md:h-[537px] p-4 relative flex justify-center md:block">
+                <img src="img/mais_azul.svg" className="absolute top-0 left-0 w-36 hidden md:flex" alt="" />
+                <img src="img/Andressa.jpg" className="md:absolute bottom-0 right-0 w-80 h-[450px] object-cover" alt="" />
+              </div>
+              <div className="sm:w-[395px] p-4">
+                <h1 style={{ fontFamily: "Alice", fontSize: "36px" }}>Andressa Belo</h1>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour
+                  There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</p>
+              </div>
+            </div>
+            <div className="flex items-center md:-mt-20  flex-col justify-center md:flex-row">
 
+              <div className="sm:w-[395px] p-4">
+                <h1 style={{ fontFamily: "Alice", fontSize: "36px" }}>Monica Goes</h1>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour
+                  There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</p>
+              </div>
+              <div className="sm:w-[395px] md:h-[537px] p-4 relative flex justify-center sm:block">
+                <img src="img/mais_branco.svg" className="absolute top-0 right-0 w-36 hidden md:flex" alt="" />
+                <img src="img/Monica.jpg" className="md:absolute bottom-0 left-0 w-80 h-[450px] object-cover" alt="" />
+              </div>
+            </div>
+
+          </div>
+          <Button variant={"inverseTwo"} size={"lg"}>Saiba mais <HiArrowSmRight /></Button>
+          <div>
+
+          </div>
         </div>
       </div>
     </div>
