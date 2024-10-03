34e08c66b89a2aa6b862c206c29afb1bf4c57ae1
anabsena <anabsenaf@gmail.com>
2024-10-02 21:00:51 -0300

    ok

--- src/screens/Public/AboutUs/AboutUs.tsx
+++ src/screens/Public/AboutUs/AboutUs.tsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
     return (
         <div className=" bg-[#F2F4FF]">
             <div className="w-full relative min-h-screen overflow-hidden ">
-                <div className="   lg:bg-[#545C99]  lg:w-full lg:h-28">
+                <div className="lg:bg-[#545C99]  lg:w-full lg:h-28">
 
                     <HeaderHome />
                 </div>
